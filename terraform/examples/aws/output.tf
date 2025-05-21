7ed2adad8eb1f337cb5ed075e854b9d19754c10f
Kieran Potts <hello@kieranpotts.com>
2025-05-21 09:43:24 +0100

    consolidate terraform aws docs

--- terraform/examples/aws/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.app_instance.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_instance.public_ip
-}
